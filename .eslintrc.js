e3c9d5c2b1e1d82171f6b66e173a99bfd71a89f9
nikitabelo4ka <belko.nikita@yandex.ru>
2022-04-28 14:44:41 +0300

    Lesson-1 done.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+    },
+    extends: ['airbnb', 'plugin:jest/recommended', 'jest-enzyme'],
+    plugins: [
+        'label',
+        'import',
+        'react',
+    ],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true // для React
+        }
+    }
+}
