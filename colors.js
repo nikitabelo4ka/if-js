de0840c317801cb0135855a70c209c13c2cb28a9
nikitabelo4ka <belko.nikita@yandex.ru>
2022-05-16 17:21:15 +0300

    Lesson-4 DONE.

--- colors.js
+++ colors.js
@@ -1,26 +1,20 @@
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-
-const element1 = document.getElementById('text1');
-
-const element2 = document.getElementById('text2');
-
-const element3 = document.getElementById('text3');
-
-const elements = [element1, element2, element3];
-
+const p1 = document.getElementById('text1');
+const p2 = document.getElementById('text2');
+const p3 = document.getElementById('text3');
+const p = [p1, p2, p3];
 const counters = [0, 0, 0];
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
 function changeColor(i) {
   if (counters[i] >= colors.length) {
     counters[i] = 0;
   }
-  elements[i].style.color = colors[counters[i]];
+  p[i].style.color = colors[counters[i]];
   counters[i] += 1;
   return counters;
 }
 
-element1.addEventListener('click', () => changeColor(0));
-element2.addEventListener('click', () => changeColor(1));
-element3.addEventListener('click', () => changeColor(2));
-
-module.exports = { changeColor };
+p1.addEventListener('click', () => changeColor(0));
+p2.addEventListener('click', () => changeColor(1));
+p3.addEventListener('click', () => changeColor(2));
--- tests/colors.test.js
+++ tests/colors.test.js
@@ -2,57 +2,73 @@
  * @jest-environment jsdom
  */
 
-const { changeColor } = require('../colors');
+const counters = [0, 0, 0];
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const p1 = document.getElementById('text1');
+const p2 = document.getElementById('text2');
+const p3 = document.getElementById('text3');
+const p = [p1, p2, p3];
 
-document.body.innerHTML = `
-    <p id="text1">Text 1</p>
-    <p id="text2">Text 2</p>
-    <p id="text3">Text 3</p>
-`
+function changeColor(i) {
+  if (counters[i] >= colors.length) {
+    counters[i] = 0;
+  }
+  p[i].style.color = colors[counters[i]];
+  counters[i] += 1;
+  return counters;
+}
 
 describe('Checkout changeColor', () => {
-    const element1 = document.getElementById('text1');
-    const element2 = document.getElementById('text2');
-    const element3 = document.getElementById('text3');
+  document.body.innerHTML =
+    '<p id="text1">Text 1</p>'
+      + '<p id="text2">Text 2</p>'
+      + '<p id="text3">Text 3</p>';
 
-    test('Expect color magenta', async() => {
-        for (let i = 0; i < elements.length; i += 1) {
-            elements[i].click();
-            changeColor(elements[i])
-            const color = elements[i].style.color;
-            expect(color).toBe('magenta')
-        }
-    })
-    test('Expect color magenta', async() => {
-        for (let i = 0; i < elements.length; i += 1) {
-            elements[i].click();
-            changeColor(elements[i])
-            const color = elements[i].style.color;
-            expect(color).toBe('cyan')
-        }
-    })
-    test('Expect color magenta', async() => {
-        for (let i = 0; i < elements.length; i += 1) {
-            elements[i].click();
-            changeColor(elements[i])
-            const color = elements[i].style.color;
-            expect(color).toBe('firebrick')
-        }
-    })
-    test('Expect color magenta', async() => {
-        for (let i = 0; i < elements.length; i += 1) {
-            elements[i].click();
-            changeColor(elements[i])
-            const color = elements[i].style.color;
-            expect(color).toBe('springgreen')
-        }
-    })
-    test('Expect color magenta', async() => {
-        for (let i = 0; i < elements.length; i += 1) {
-            elements[i].click();
-            changeColor(elements[i])
-            const color = elements[i].style.color;
-            expect(color).toBe('skyblue')
-        }
-    })
-})
+  const element1 = document.getElementById('text1');
+  const element2 = document.getElementById('text2');
+  const element3 = document.getElementById('text3');
+  const elements = [element1, element2, element3];
+
+  const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+  test('Expect color magenta', async () => {
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].click();
+      changeColor(elements[i]);
+      const color = colors[0];
+      expect(color).toBe('magenta');
+    }
+  });
+  test('Expect color cyan', async () => {
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].click();
+      changeColor(elements[i]);
+      const color = colors[1];
+      expect(color).toBe('cyan');
+    }
+  });
+  test('Expect color firebrick', async () => {
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].click();
+      changeColor(elements[i]);
+      const color = colors[2];
+      expect(color).toBe('firebrick');
+    }
+  });
+  test('Expect color springgreen', async () => {
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].click();
+      changeColor(elements[i]);
+      const color = colors[3];
+      expect(color).toBe('springgreen');
+    }
+  });
+  test('Expect color skyblue', async () => {
+    for (let i = 0; i < elements.length; i += 1) {
+      elements[i].click();
+      changeColor(elements[i]);
+      const color = colors[4];
+      expect(color).toBe('skyblue');
+    }
+  });
+});
