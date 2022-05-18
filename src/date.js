c5fc537b62af33455e848e3d5117e3404aaa5542
nikitabelo4ka <belko.nikita@yandex.ru>
2022-05-18 22:19:06 +0300

    Lesson-5 done.

---
+++ src/date.js
@@ -0,0 +1,57 @@
+const date = '2020-11-26';
+
+function newDate(str) {
+  console.log(str.split('-').reverse().join('.'));
+}
+
+newDate(date);
+
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+function search(str) {
+  const result = data.filter((object) => Object.values(object).includes(str));
+  result.forEach((item) => console.log(Object.values(item).join(', ')));
+}
+
+search('Berlin');
