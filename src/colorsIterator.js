29f63b193856038e580113955dc03c155c1e354b
nikitabelo4ka <belko.nikita@yandex.ru>
2022-06-14 13:20:07 +0300

    Lesson-9 done. v.2

--- src/colorsIterator.js
+++ src/colorsIterator.js
@@ -7,13 +7,16 @@ const colors = {
         const value = this.data[index += 1];
         return { value, done: false };
       }
+
       if (index === this.data.length) {
         index = 0;
         const value = this.data[index += 1];
         return { value, done: false };
       }
+
       return { done: true };
     };
+
     return { next };
   },
 };
@@ -21,6 +24,7 @@ const colors = {
 for (const paragraphs of document.getElementsByTagName('p')) {
   let i = 0;
   const colorsIterator = colors[Symbol.iterator]();
+
   paragraphs.onclick = function () {
     this.style.color = colorsIterator.next().value;
     i += 1;
