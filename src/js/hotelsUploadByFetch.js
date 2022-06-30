0bc6422c2100018edf62e067276b5f472df1914f
nikitabelo4ka <belko.nikita@yandex.ru>
2022-06-30 15:26:11 +0300

    Lesson-12 done. V.2

--- src/js/hotelsUploadByFetch.js
+++ src/js/hotelsUploadByFetch.js
@@ -1,12 +1,18 @@
 const homesGuestsLoves = document.getElementById('homes-guests-loves');
 
-fetch('https://fe-student-api.herokuapp.com/api/hotels/popular')
+const API = 'https://fe-student-api.herokuapp.com/api';
+
+const PATH = 'hotels/popular';
+
+fetch(`${API}/${PATH}`)
   .then((response) => response.json())
   .then((result) => {
     result.forEach((item) => {
       const newDiv = document.createElement('div');
       newDiv.className = 'homes-guests-loves-element col-m-6 col-3';
       homesGuestsLoves.appendChild(newDiv);
-      newDiv.innerHTML = `<img class="hotel-icon" src="${item.imageUrl}" alt="${item.name} image"/><a class="hotel-link" href="">${item.name}</a><p class="hotel-location">${item.city}, ${item.country}</p>`;
+      newDiv.innerHTML = `<img class="hotel-icon" src="${item.imageUrl}" alt="${item.name} image"/>
+                          <a class="hotel-link" href="">${item.name}</a>
+                          <p class="hotel-location">${item.city}, ${item.country}</p>`;
     });
   });
