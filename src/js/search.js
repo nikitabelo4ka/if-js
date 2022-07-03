d0d881b6a4c3e2c49dd6f6aa03419dda473b33be
nikitabelo4ka <belko.nikita@yandex.ru>
2022-07-03 16:38:19 +0300

    Lesson-13 done. v.2

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,5 @@
 const form = document.getElementById('inputs');
-
 let hotelsExist = false;
-
 const availableHotelsWrap = document.getElementsByClassName('available-hotels')[0];
 
 function makeStartState() {
@@ -20,38 +18,36 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const destinationInput = document.getElementById('destination');
-
-  const destinationInputValue = destinationInput.value.replaceAll(' ', '%20');
-
+  const destinationInputValue = destinationInput.value;
   const API = 'https://fe-student-api.herokuapp.com/api';
-
   const PATH = 'hotels?search';
 
   fetch(`${API}/${PATH}=${destinationInputValue}`)
     .then((response) => response.json())
     .then((result) => {
-      if (result.length !== 0) {
-        availableHotelsWrap.classList.remove('unactive');
-
-        makeStartState();
-
-        result.forEach((item) => {
-          const availableHotels = document.getElementById('available-hotels');
-          const newDiv = document.createElement('div');
-          newDiv.className = 'available-hotels-element col-m-6 col-3';
-          availableHotels.appendChild(newDiv);
-          newDiv.innerHTML = `<img class="available-hotels-icon" src="${item.imageUrl}" alt="${item.name} image"/>
-                            <a class="available-hotels-link" href="">${item.name}</a>
-                            <p class="available-hotels-location">${item.city}, ${item.country}</p>`;
-        });
-
-        hotelsExist = true;
-      } else {
+      if (result.length === 0) {
         availableHotelsWrap.classList.add('unactive');
 
         makeStartState();
 
-        alert('По вашему запросу ничего не найдено');
+        alert('Nothing was found for your query');
+
+        return;
       }
+      availableHotelsWrap.classList.remove('unactive');
+
+      makeStartState();
+
+      result.forEach((item) => {
+        const availableHotels = document.getElementById('available-hotels');
+        const newDiv = document.createElement('div');
+        newDiv.className = 'available-hotels-element col-m-6 col-3';
+        availableHotels.appendChild(newDiv);
+        newDiv.innerHTML = `<img class="available-hotels-icon" src="${item.imageUrl}" alt="${item.name} image"/>
+                            <a class="available-hotels-link" href="">${item.name}</a>
+                            <p class="available-hotels-location">${item.city}, ${item.country}</p>`;
+      });
+
+      hotelsExist = true;
     });
 });
